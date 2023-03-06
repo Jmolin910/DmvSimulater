5c0edd1b904f3446fcf98e4a8a0de8ef1eea0b91
Jmolin910 <jamolin@quinnipiac.edu>
2023-03-06 17:22:30 -0500

    second step, gives out a number and makes them wait

--- src/DmvSimulater.java
+++ src/DmvSimulater.java
@@ -1,5 +1,26 @@
+
 public class DmvSimulater {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+
+        int waitTime = (int)(100 * Math.random() + 1);
+
+        System.out.println("Your call number is " + waitTime + ". Please wait your turn patiently.");
+
+        int counter = waitTime;
+        do{
+            counter++;
+            
+            System.out.println("Now serving: " + counter);
+
+            if (counter == 100){
+                counter = 0;
+
+            }
+            Thread.sleep(100);
+        }while (counter != waitTime);
+
+        System.out.println("Hello and welcome to the DMV. Can i please see your forms and required information");
+        System.out.println("Sorry this is not the correct information please come back when you have it all.");
 
     }
 }
