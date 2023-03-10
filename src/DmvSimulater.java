e7ea3e7467da6b0f299d418ca91bac8be9e388a8
Jmolin910 <jamolin@quinnipiac.edu>
2023-03-09 20:15:37 -0500

    Added one percent miracle chance

--- src/DmvSimulater.java
+++ src/DmvSimulater.java
@@ -3,6 +3,7 @@ public class DmvSimulater {
     public static void main(String[] args) throws InterruptedException {
 
         int waitTime = (int)(200 * Math.random() + 1);
+        int miracle = (int)(100 * Math.random());
 
         System.out.println("Your call number is " + waitTime + ". Please wait your turn patiently.");
 
@@ -19,8 +20,15 @@ public class DmvSimulater {
             Thread.sleep(100);
         }while (counter != waitTime);
 
-        System.out.println("Hello and welcome to the DMV. Can I please see your forms and required information?");
-        System.out.println("Don't care.");
+        if (miracle == 83)
+        {
+            System.out.println("Its your lucky day! you have all your paper work, your request will be processed in about a week.");
+        }
+        else
+        {
+            System.out.println("Hello and welcome to the DMV. Can I please see your forms and required information?");
+            System.out.println("Don't care.");
+        }
 
     }
 }
